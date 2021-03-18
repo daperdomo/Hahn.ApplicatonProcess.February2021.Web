786c86e4e253dbb487bfc651916c38b045c5a6f9
Darwin Perdomo <darwin.p.c@hotmail.com>
2021-03-18 18:49:36 -0400

    Implementing Fluent validations

---
+++ Hahn.ApplicatonProcess.February2021.Domain/Validators/AssetValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Hahn.ApplicatonProcess.February2021.Domain.Models;
+using Hahn.ApplicatonProcess.February2021.Domain.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.February2021.Domain.Validators
+{
+    public class AssetValidator : AbstractValidator<AssetViewModel>
+    {
+        public AssetValidator()
+        {
+            RuleFor(m => m.AssetName).NotEmpty();
+            RuleFor(m => m.CountryOfDepartment).NotEmpty();
+            RuleFor(m => m.EMailAdressOfDepartment).NotEmpty();
+        }
+    }
+}
